6e7ac273b9e16e924a3c335d7a0f711ed1fffd41
nakkim <kdkeiie8@gmail.com>
2023-12-04 22:09:42 +0900

    feat: 설문지 내용 수정

--- src/template/template.controller.ts
+++ src/template/template.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { ApiBody, ApiOperation, ApiQuery, ApiResponse } from '@nestjs/swagger';
 import { TemplateStatus } from '../entity/template.entity';
 import { CreateTemplateDto, CreateTemplateResponseDto } from './dto/create-template.dto';
 import { TemplateDto } from './dto/template.dto';
+import { UpdateTemplateDto } from './dto/update-template.dto';
 import { TemplateService } from './template.service';
 
 @Controller('template')
@@ -23,5 +24,13 @@ export class TemplateController {
   @ApiResponse({ status: 201, type: CreateTemplateResponseDto })
   async createTemplate(@Body() dto: CreateTemplateDto) {
     return this.templateService.createTemplate(dto);
+  }
+
+  @Patch(':id')
+  @ApiOperation({ description: '설문지 내용 수정' })
+  @ApiBody({ type: UpdateTemplateDto })
+  @ApiResponse({ status: 200, type: TemplateDto })
+  async updateTemplate(@Param('id') id: number, @Body() dto: UpdateTemplateDto) {
+    return this.templateService.updateTemplate(id, dto);
   }
 }
--- src/template/template.service.ts
+++ src/template/template.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Template, TemplateStatus } from '../entity/template.entity';
 import { CreateTemplateDto } from './dto/create-template.dto';
+import { UpdateTemplateDto } from './dto/update-template.dto';
 
 @Injectable()
 export class TemplateService {
@@ -22,5 +23,21 @@ export class TemplateService {
     this.logger.log(`설문지 생성: ${JSON.stringify(template)}`);
 
     return template;
+  }
+
+  async updateTemplate(id: number, dto: UpdateTemplateDto) {
+    const template = await this.templateRepository.findOneBy({ id });
+
+    if (!template) {
+      throw new BadRequestException('설문지가 존재하지 않습니다.');
+    }
+    if (template.status !== TemplateStatus.WAITING) {
+      throw new BadRequestException('대기중 상태의 설문지만 수정할 수 있습니다.');
+    }
+
+    const updatedTemplate = await this.templateRepository.save({ ...template, ...dto });
+    this.logger.log(`설문지 수정: ${JSON.stringify(updatedTemplate)}`);
+
+    return updatedTemplate;
   }
 }
