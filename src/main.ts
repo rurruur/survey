6a2b855337ab29c0846b6e7b7de00cf71f6c3b4e
nakkim <kdkeiie8@gmail.com>
2023-12-04 21:54:13 +0900

    feat: validation 파이프 글로벌 설정

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import setupSwagger from './config/swagger';
@@ -6,6 +7,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   await setupSwagger(app);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   await app.listen(3000);
 }
