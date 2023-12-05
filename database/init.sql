83046a6e4491f85ad85f6c8929981dbabe8f8a1b
nakkim <kdkeiie8@gmail.com>
2023-12-05 23:19:16 +0900

    db: 설문지 문항 테이블 생성

--- database/init.sql
+++ database/init.sql
@@ -10,3 +10,16 @@ create table template (
 	updated_at timestamp not null default CURRENT_TIMESTAMP,
 	deleted_at timestamp
 );
+
+-- 설문지 문항 테이블 생성
+create table question (
+	template_id serial not null,
+	question_number int not null,
+	content text not null,
+	answers jsonb not null,
+	created_at timestamp not null default CURRENT_TIMESTAMP,
+	updated_at timestamp not null default CURRENT_TIMESTAMP,
+	deleted_at timestamp,
+	primary key (template_id, question_number),
+	foreign key (template_id) references template(id)
+);
